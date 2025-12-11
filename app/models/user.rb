eb334ed4adc1ac8802b7ae27a68262b6c4ef057e
Nicholas Alves <nicholas_jpa@hotmail.com>
2025-12-11 15:35:18 +0000

    Scope Category to current_user; add user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Devise modules (already present)
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # add associations
+  has_many :categories, dependent: :destroy
+  has_many :expenses, dependent: :destroy
+  has_many :budgets, dependent: :destroy
 end
+
